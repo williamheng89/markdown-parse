9faa5b28a9d3c370f60f6f1f08e3d6f97fb33da4
williamheng89 <williamheng89@gmail.com>
2022-02-25 16:14:01 -0800

    reverted

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -14,14 +14,14 @@ public class MarkdownParse {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             //if [ open bracket is not found, there are no links thus return.
             if (nextOpenBracket == -1){
-                continue;
+                return toReturn;
             }
             if (markdown.indexOf("!") == nextOpenBracket-1){
-                continue;
+                return toReturn;
             }
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             if (nextCloseBracket == -1){
-                continue;
+                return toReturn;
             }
             /**
              * attempt to fix nested parenthesis, brackets, escaped brackets
@@ -32,14 +32,14 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", nextCloseBracket);
             //if ( open parenthesis is not found, there are no links thus return.
             if (openParen == -1){
-                continue;
+                return toReturn;
             }
             if (openParen != nextCloseBracket+1){
                 return toReturn;
             }
             int closeParen = markdown.indexOf(")", openParen);
             if (closeParen == -1){
-                continue;
+                return toReturn;
             }
             /**
             * attempt to fix backTicks
